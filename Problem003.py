419a06e9e0326350254e76612adaf73bf696ef3f
Amertz08 <adammertz@gmail.com>
2015-04-10 18:50:15 -0500

    refactored problem003 to library

--- Problem003.py
+++ Problem003.py
@@ -1,57 +1,19 @@
-'''
-Takes in a value
-returns true if prime
-'''
-def isPrime(n):
-    #Assume that it is prime
-    answer = True
-    #Get next factor
-    a = n - 1
-    #Iterate over all possible factors
-    while(a > 1):
-        #See if value is a factor
-        if(n%a == 0):
-            answer = False
-            #Exit if true
-            break
-        a -= 1
-    return answer
+from ProjectEuler import find_factors, is_prime
 
-'''
-Takes in a value
-returns list of all the factors
-'''
-def findFactors(n):
-    #Create an empty list
-    f = []
-    #Look at the next lower value
-    a = n - 1
-    #Iterate through all values
-    while(a > 1):
-        #Determine if it is a factor
-        if(n%a == 0):
-            #Add factor to list
-            f.append(a)
-        a -= 1
-    return f
-
-'''
-Program
-'''
 print('This program will find the highest prime factor of a number')
 value = int(input('Enter a value: '))
 
-#Get all factors for value
-factors = findFactors(value)
+#   Get all factors for value
+factors = find_factors(value)
 
-#Iterate over factors highest to lowest
-#Exit if prime
+#   Iterate over factors highest to lowest
+#   Exit if prime
 n = 0
-while(not isPrime(factors[n])):
-    if(isPrime(factors[n])):
-        break
+while(n < len(factors) and not is_prime(factors[n])):
     n += 1
 
-#Output results
-print('Highest prime: %s' % factors[n])
-    
+#   Output results
+if(n < len(factors)):  #   n < 0 if no prime factors
+    print('Highest prime: %d' % factors[n])
+else:
+    print('There is no prime factor')
--- ProjectEuler.py
+++ ProjectEuler.py
@@ -6,3 +6,45 @@ def Fibonacci(n):
     if n == 0: return 0
     elif n == 1: return 1
     else: return Fibonacci(n-1)+Fibonacci(n-2)
+
+
+'''
+n       :   int
+return  :   boolean
+Takes in a value n
+returns True if prime
+'''
+def is_prime(n):
+    #   Assume that it is prime
+    answer = True
+    #   Get next factor
+    a = n - 1
+    #Iterate over all possible factors
+    while(a > 1):
+        #See if value is a factor
+        if(n%a == 0):
+            answer = False
+            #Exit if true
+            break
+        a -= 1
+    return answer
+
+'''
+n       :   int
+return  :   list
+Takes in a value n
+returns list of all the factors
+'''
+def find_factors(n):
+    #Create an empty list
+    f = []
+    #Look at the next lower value
+    a = n - 1
+    #Iterate through all values
+    while(a > 1):
+        #Determine if it is a factor
+        if(n%a == 0):
+            #Add factor to list
+            f.append(a)
+        a -= 1
+    return f
