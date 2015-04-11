09bed79a72c3cc1a7e1d0f8af439fa3c77dae6ea
Amertz08 <Amertz08@users.noreply.github.com>
2015-04-11 12:47:03 -0500

    Finished Problem022

--- Problem022.py
+++ Problem022.py
@@ -1,32 +1,52 @@
+#   This is not done.
+
 #Take in Names
 with open('p022_names.txt') as f:
     for a in f:
-        val = a
+        names = a
 
-#print(val)
-names = []
-name = ''
-for a in val:
-    start = 0
 
-    #Increment count if quote
-    if(a == '"'):
-        start += 1
+#   Clean names
+names = names.split(',')
+clean_names = [name.strip('"') for name in names]
+clean_names = sorted(clean_names)
 
-    #Add value
-    if(start%2 == 0):
-        name.append(a)
+#   Empty score list
+scores = [0 for name in clean_names]
 
-    #Add name to list
-    if(start%2 != 0):
-        #print(name)
-        #Add name to list
-        names.append(name)
-        #Reset Name
-        name = []
+#   Each letters associated score
+alpha_points = {
+'A' :   1,
+'B' :   2,
+'C' :   3,
+'D' :   4,
+'E' :   5,
+'F' :   6,
+'G' :   7,
+'H' :   8,
+'I' :   9,
+'J' :   10,
+'K' :   11,
+'L' :   12,
+'M' :   13,
+'N' :   14,
+'O' :   15,
+'P' :   16,
+'Q' :   17,
+'R' :   18,
+'S' :   19,
+'T' :   20,
+'U' :   21,
+'V' :   22,
+'W' :   23,
+'X' :   24,
+'Y' :   25,
+'Z' :   26
+}
 
-print(names)
-#Sort alphabetically
-#Calculate alphabetical value
-#Calculate name scores
-#Sum all scores
+for a in range(len(clean_names)):
+    for b in range(len(clean_names[a])):
+        scores[a] += alpha_points[clean_names[a][b]] * (a + 1)
+
+print 'name: %s score: %d' % (clean_names[937], scores[937])
+print 'Total sum: %d' % sum(scores)
