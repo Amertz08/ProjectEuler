d50d93de0fdb2c6f78713a35c29d7b76fb03c5a5
Amertz08 <Amertz08@users.noreply.github.com>
2015-04-12 18:58:36 -0500

    simplified variables in problem 35

--- Problem035.py
+++ Problem035.py
@@ -6,13 +6,14 @@ primes = [x for x in range(2,max_val + 1) if is_prime(x)]
 print 'Found primes'
 def is_circular_prime(val):
     c = 0
-    for a in range(len(str(val))):
-        y = str(val % (1 * 10**(len(str(val)) - 1)))
-        x = str(val / (1 * 10**(len(str(val)) - 1)))
+    d = len(str(val))
+    for a in range(d):
+        y = str(val % (1 * 10**(d - 1)))
+        x = str(val / (1 * 10**(d - 1)))
         val = int(y + x)
         if(is_prime(val)): c += 1
 
-    if c < len(str(val)):   return False
+    if c < d:   return False
     else:   return True
 
 circle_primes = [prime for prime in primes if is_circular_prime(prime)]
