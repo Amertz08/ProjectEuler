fbf5aebf2665e673e9bde1a07f3a39df9df1b92b
Amertz08 <Amertz08@users.noreply.github.com>
2015-08-19 17:00:47 -0500

    added input file, update for pythonic solution

--- Python/Problem013.py
+++ Python/Problem013.py
@@ -1,9 +1,7 @@
 with open("../p013.txt") as f:
     content = f.readlines()
 
-answer = 0
-for num in content:
-    answer += int(num)
+answer = sum([int(num) for num in content])
 
 assert int(str(answer)[:10]) == 5537376230, "fail"
 print "pass"
